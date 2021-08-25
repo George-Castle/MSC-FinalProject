c1352a5689b3ed7189ef43ed7793a29070140955
George Castle <64536282+George-Castle@users.noreply.github.com>
2021-08-25 14:58:07 +0100

    Update and rename OriginalGetCartVectors.m to AdjustedGetCartVectors.m

--- MATLAB/AdjustedGetCartVectors.m
+++ MATLAB/AdjustedGetCartVectors.m
@@ -1,23 +1,13 @@
+
+% adjusted Ionis Agtzidis script to make rotation work
 % GetCartVectors.m
 %
 % This function returns an nx3 arrays of cartesian vectors for the eye direction
 % within the FOV of the headset, the eye direction in the world coordinates and
 % the head direction in the world.
 %
-% input:
-%   data        - ARFF data
-%   metadata    - metadata of ARFF data
-%   attributes  - attributes of ARFF data
-%
-% output:
-%   eyeFovVec   - nx3 (x,y,z) array corresponding to FOV vector of gaze centered 
-%                 around the middle of the video corresponding to (1,0,0) vector
-%   eyeHeadVec  - nx3 (x,y,z) array corresponding to the direction of the eye in 
-%                 the world. That is the head+eye position
-%   headVec     - nx3 (x,y,z) array corresponding to the direction of the head in
-%                 the world
 
-function [eyeFovVec, eyeHeadVec, headVec] = OriginalGetCartVectors(data, metadata, attributes)
+function [eyeFovVec, eyeHeadVec, headVec, angleHeadRads] = GetCartVectors(data, metadata, attributes)
     c_xName = 'x';
     c_yName = 'y';
     c_xHeadName = 'x_head';
@@ -31,6 +21,7 @@ function [eyeFovVec, eyeHeadVec, headVec] = OriginalGetCartVectors(data, metadat
     angleHeadInd = GetAttPositionArff(attributes, c_angleHeadName);
 
     eyeFovVec = zeros(size(data,1),3);
+    
     eyeHeadVec = zeros(size(data,1),3);
     headVec = zeros(size(data,1),3);
 
@@ -40,18 +31,20 @@ function [eyeFovVec, eyeHeadVec, headVec] = OriginalGetCartVectors(data, metadat
         headVec(ind,:) = curHeadVec;
 
         angleHeadRads = data(ind, angleHeadInd) * pi / 180;
-        videoVec = [-1; 0; 0]; % middle of the video is considered its center
 
-        rot = HeadToVideoRot(curHeadVec, angleHeadRads, videoVec);
-        display(rot);
-        rot = rot';
+        rot = fullRotation(headVec, angleHeadRads);
 
         [horGazeRads, verGazeRads] = EquirectToSpherical(data(ind, xInd), data(ind, yInd), metadata.width_px, metadata.height_px);
+
+
         gazeVec = SphericalToCart(horGazeRads, verGazeRads);
+
+        
+
         eyeHeadVec(ind,:) = gazeVec;
 
-        gazeWithinVec = RotatePoint(rot, gazeVec);
 
-        eyeFovVec(ind,:) = gazeWithinVec(:);
+        eyeFovVec = rot * [eyeHeadVec(1,1);eyeHeadVec(1,3);eyeHeadVec(1,2)]; % LOCAL TO [0,1,0]
+        eyeFovVec = eyeFovVec';
     end
 end
